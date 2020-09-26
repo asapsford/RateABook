76b9f7cfa4a6554afbf0bb90d0c9f68bafe7becd
Alex Sapsford <asapsford11@gmail.com>
2020-09-26 14:20:36 +0100

    Initial Commit

---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Alex Sapsford on 26/09/2020.
+//  Copyright © 2020 Alex Sapsford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
